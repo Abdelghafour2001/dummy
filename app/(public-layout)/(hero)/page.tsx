eae7cd104a2452b98ae928d21d260d3bff65deb6
abdelghafour lahrache <abdelghafourlahrache@gmail.com>
2024-05-20 15:37:16 +0100

    front

--- app/(public-layout)/(hero)/page.tsx
+++ app/(public-layout)/(hero)/page.tsx
@@ -5,14 +5,14 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center gap-5 justify-center h-[70vh]">
       <h3 className="text-4xl font-bold text-center">
-        Welcome to OpenChat! Your Gateway to Intelligent Conversations, Powered
-        by OpenAPI
+        Welcome to Capgemini Engineering Assistant! Your Gateway to Intelligent Conversations, Powered
+        by Retrieval Augmented Generations
       </h3>
       <p className="sm:w-[75%] mx-auto text-center text-muted-foreground ">
-        OpenChat redefines the chat experience by seamlessly integrating the
-        robust capabilities of the OpenAPI. This platform takes conversational
+        The assistant redefines the chat experience by seamlessly integrating the
+        robust capabilities of the latest opensource LLMs. This platform takes conversational
         AI to new heights, offering you a dynamic and intelligent chat companion
-        designed for a wide array of applications.
+        designed for a wide array of applications in Engineering fields.
       </p>
       <Link href="/register" className={buttonVariants({ size: "lg" })}>
         Get started
